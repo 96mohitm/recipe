f37bf9e767a61b421a71b475713d0f82b2554fdf
Mohit Musaddi <96mohitm@gmail.com>
2017-07-15 16:15:56 +0530

    Application setup

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { RecipePage } from './app.po';
 
-describe('recipe App', () => {
+describe('recipe App', function() {
   let page: RecipePage;
 
   beforeEach(() => {
     page = new RecipePage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
